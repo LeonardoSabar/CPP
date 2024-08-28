fe6d9bd0eb8667c0c5b2476a8a70c3bbbcd42eec
Leonardo Barbosa Sales <leobarbo@c1r5p1.42sp.org.br>
2024-08-28 11:42:54 -0300

    Funciona, mas não esta utilizando uppercase

---
+++ ex00/src/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int	main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		string	line;
+		unsigned long words;
+		for (words = 1; argv[words] != '\0'; words++)
+		{
+			line = argv[words];
+			unsigned long i;
+			for(i = 0 ; i < line.length(); i++)
+			{
+				if (line[i] >= 'a' && line[i] <= 'z')
+					line[i] = line[i] - 32;
+			}
+			cout << line;
+		}
+		cout << endl;
+		}
+		else
+			cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	return 0;
+}
